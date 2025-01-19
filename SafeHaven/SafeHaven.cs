63c4ecd8f0da65071c360331d422753269ef5d05
digitalfr4nk <79330257+digitalfr4nk@users.noreply.github.com>
2025-01-19 13:29:40 +0000

    Added SafeHaven class

---
+++ SafeHaven/SafeHaven.cs
@@ -0,0 +1,22 @@
+namespace SafeHaven
+{
+    /// <summary>
+    /// A class that represents the SafeHaven application.
+    /// </summary>
+    public class SafeHaven
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SafeHaven"/> class.
+        /// </summary>
+        public SafeHaven()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the title of the application.
+        /// </summary>
+        public string Title => "SafeHaven";
+    }
+}
